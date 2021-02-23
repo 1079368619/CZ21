1b15f72967d4a2c69ef08410a5b77c30271076d1
1079368619 <1079368619@qq.com>
2021-02-23 15:14:49 +0800

    1

---
+++ CZ21-S3-Ply-sq-music/src/main/java/com/yc/sq/MusicApp.java
@@ -0,0 +1,13 @@
+package com.yc.sq;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MusicApp {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MusicApp.class, args);
+	}
+	
+}
